90fa664573496e418b36a6b11cfd6ad62a10527e
Mitch Hiltz <mitch.hiltz@gmail.com>
2020-08-10 15:54:15 -0400

    provisional script complete

--- provisionResources.ps1
+++ provisionResources.ps1
@@ -1,35 +1,44 @@
 # TODO: set variables
-$studentName = ""
-$rgName = ""
-$vmName = ""
-$vmSize = ""
-$vmImage = ""
-$vmAdminUsername = ""
+$studentName = "mitch"
+$rgName = "$studentName-lc0820-ps-rg"
+$vmName = "$studentName-lc0820-ps-vm"
+$vmSize = "Standard_B2s"
+$vmImage = $(az vm image list --query "[? contains(urn, 'Ubuntu')] | [0].urn")
+$vmAdminUsername = "student"
+
 $kvName = "$studentName-lc0820-ps-kv"
 $kvSecretName = "ConnectionStrings--Default"
 $kvSecretValue = "server=localhost;port=3306;database=coding_events;user=coding_events;password=launchcode"
 
-# TODO: provision RG
+az configure --default location=eastus
 
-# TODO: provision VM
+# Provision RG
+az group create -n "$rgName"
+az configure --default group=$rgName
 
-# TODO: capture the VM systemAssignedIdentity
+# Provision VM
+$vmData = $(az vm create -n $vmName --size $vmSize --image $vmImage --admin-username $vmAdminUsername --admin-password $(Read-Host "Authenticate to Azure (student)" -AsSecureString) --authentication-type password --assign-identity)
 
-# TODO: open vm port 443
+# Capture the VM systemAssignedIdentity and IP Address
+$vmId = $(az vm identity show --name $vmName --query "principalId").Trim('"')
+$vmIp = $(az vm list-ip-addresses -n $vmName --query "[0].virtualMachine.network.publicIpAddresses[0].ipAddress").Trim('"')
 
-# provision KV
+# Open vm port 443
+az vm open-port --port 443
 
+# Provision KV
 az keyvault create -n $kvName --enable-soft-delete false --enabled-for-deployment true
 
-# TODO: create KV secret (database connection string)
+# Create KV secret (database connection string)
+az keyvault secret set --vault-name $kvName --description 'connection string' --name $kvSecretName --value $kvSecretValue
 
-# TODO: set KV access-policy (using the vm ``systemAssignedIdentity``)
+# Set KV access-policy (using the vm ``systemAssignedIdentity``)
+az keyvault set-policy --name $kvName --object-id $vmId --secret-permissions list get
 
+# Send 3 bash scripts to the VM using az vm run-command invoke
 az vm run-command invoke --command-id RunShellScript --scripts @vm-configuration-scripts/1configure-vm.sh
-
 az vm run-command invoke --command-id RunShellScript --scripts @vm-configuration-scripts/2configure-ssl.sh
-
 az vm run-command invoke --command-id RunShellScript --scripts @deliver-deploy.sh
 
-
-# TODO: print VM public IP address to STDOUT or save it as a file
+# Print VM public IP address to STDOUT or save it as a file
+Write-Host "VM available at $vmIp"
